a900090688b33917dc5808b5ff04c6302d55ebdc
alpaca0984 <alpaca0984@gmail.com>
2022-09-03 11:30:46 +0200

    feat: download PDF and save internal storage

--- android/app/src/main/java/com/example/downloadinterceptionsample/MainActivity.kt
+++ android/app/src/main/java/com/example/downloadinterceptionsample/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.downloadinterceptionsample
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Base64
+import android.webkit.JavascriptInterface
+import android.webkit.URLUtil
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -12,31 +18,91 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.downloadinterceptionsample.ui.theme.DownloadInterceptionSampleTheme
+import java.io.File
+
+private const val JS_INTERFACE_NAME = "android"
 
 class MainActivity : ComponentActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val javaScriptInterface = JavaScriptInterfaceImpl(context = this)
         setContent {
             DownloadInterceptionSampleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(Modifier.fillMaxWidth()) {
+                    Column(modifier = Modifier.padding(18.dp)) {
                         Text(
-                            text = "↓ is a WebView",
-                            style = MaterialTheme.typography.h2,
+                            text = "WebView File Download Interception",
+                            style = MaterialTheme.typography.h4,
                         )
 
-                        Spacer(Modifier.height(12.dp))
+                        Spacer(Modifier.height(24.dp))
                         AndroidView(factory = { context ->
                             WebView(context).apply {
+                                settings.javaScriptEnabled = true
+                                webViewClient = WebViewClient()
+                                addJavascriptInterface(javaScriptInterface, JS_INTERFACE_NAME)
+                                setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+                                    if (url.startsWith("blob:")) {
+                                        val script = JavaScriptInterfaceImpl.fetchBlobScript(
+                                            blobUrl = url,
+                                            mimetype = mimetype
+                                        )
+                                        evaluateJavascript(script, null)
+                                    }
+                                }
                                 loadUrl("http://10.0.2.2:3000")
                             }
                         })
                     }
                 }
             }
+        }
+    }
+}
+
+class JavaScriptInterfaceImpl(
+    private val context: Context
+) {
+
+    @JavascriptInterface
+    fun receiveBase64(base64: String, url: String, mimetype: String) {
+        val content = Base64.decode(base64, Base64.DEFAULT)
+        val fileName = URLUtil.guessFileName(url, "", mimetype)
+        val file = File(context.filesDir, fileName)
+        file.writeBytes(content)
+
+//        val intent = Intent(Intent.ACTION_VIEW).apply {
+//            setDataAndType(Uri.fromFile(file), mimetype)
+//        }
+//        context.startActivity(intent)
+    }
+
+    companion object {
+
+        fun fetchBlobScript(blobUrl: String, mimetype: String): String {
+            return """
+                (async () => {
+                  const response = await fetch('${blobUrl}', {
+                    headers: {
+                      'Content-Type': '${mimetype}',
+                    }
+                  });
+                  const blob = await response.blob();
+
+                  const reader = new FileReader();
+                  reader.addEventListener('load', () => {
+                    const base64 = reader.result.replace(/^data:.+;base64,/, '');
+                    ${JS_INTERFACE_NAME}.receiveBase64(base64, blobUrl, mimetype);
+                  });
+                  reader.readAsDataURL(blob); 
+                })();
+            """.trimIndent()
         }
     }
 }
--- web/src/script.js
+++ web/src/script.js
@@ -1,16 +1,33 @@
+const loadLocalPdf = async (filePath) => {
+  const result = await fetch(filePath, {
+    headers: {
+      "Content-Type": "application/pdf",
+    }
+  });
+
+  return result.blob();
+};
+
 const button = document.querySelector("#button-dl");
-button.addEventListener("click", function (e) {
-  const blobStore = new Blob(["Hello-World"], {
-    type: 'text/plain'
+button.addEventListener("click", async () => {
+  const fileName = "sample.pdf";
+  const pdf = await loadLocalPdf(`./${fileName}`);
+
+  const blobStore = new Blob([pdf], {
+    type: 'application/pdf'
   });
   const data = window.URL.createObjectURL(blobStore);
   const link = document.createElement('a');
   link.href = data;
-  link.download = 'sample.txt';
+  link.download = fileName;
   document.body.appendChild(link);
 
   link.click();
 
-  window.URL.revokeObjectURL(data);
-  link.remove();
+  // NOTE: When these two lines are activated, Android can not intercept
+  // downloading because while it accesses to the blob URL, the URL is
+  // already revoked.
+  //
+  // window.URL.revokeObjectURL(data);
+  // link.remove();
 });
